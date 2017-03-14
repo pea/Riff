75715263f17d06cbee37f84506df4c1faf054f6e
Peter Bailey <prodigy.zero@gmail.com>
2017-03-14 15:28:28 +0000

    Fixed api for non-multisites and allowed endpoints to be case-insensitive

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -2,21 +2,25 @@
 
 namespace Riff\Api;
 
-use WP_POST;
-use WP_Query;
-
 class Api
 {
     public function __construct()
     {
-        $currentBlog = get_blog_details(get_current_blog_id());
+        if (function_exists('get_blog_details')) {
+            $siteUrl = get_blog_details(get_current_blog_id());
+            $siteUrl = $siteUrl->path;
+        } else {
+            $siteUrl = get_bloginfo('url');
+        }
+
         $classPath = explode('\\', get_class($this));
+
         $className = end($classPath);
         $uri = explode('/', $_SERVER['REQUEST_URI']);
         $reqName = end($uri);
-        if ($uri[1] == 'api' ||
-            (str_replace('/', '', $currentBlog->path) == $uri[1] && @$uri[2] == 'api') &&
-            $reqName == $className) {
+        if (($uri[1] == 'api' || (str_replace('/', '', $siteUrl) == $uri[1] && @$uri[2] == 'api')) &&
+            strtolower($reqName) == strtolower($className)
+        ) {
             $this->init();
             exit;
         }
